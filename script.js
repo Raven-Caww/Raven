3e38bb9fb66ac3fee10bea3810a441feb1fd85ab
Nevar <404terranotfound+@gmail.com>
2024-08-15 15:58:54 +1000

    Added some files!
    This works, right? :D

---
+++ script.js
@@ -0,0 +1,8 @@
+// Load up this cmd prompt curl command to a text file. <Done!>
+// If this is in the viewport... <Make intersection observer>
+// Print each frame one by one. <Somehow find a way to print 'frames'>
+// Repeat it. <Make this a loop>
+// Stop it when it exits the viewport. <Remove the observer class to stop it>
+
+let sdf = document.getElementById("rick");
+console.log(sdf);

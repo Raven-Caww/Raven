b2b93b7c68081290c420cccbe069555fe0c71171
TerraAU <404terranotfound+@gmail.com>
2024-09-01 19:09:04 +1000

    Birth 1.6
    Added stuff, cleaned up, requiring more content to be added and more following the rubric and stuff.

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const observeForShowAndHidden = new IntersectionObserver((elements) => {
+    console.log("Suki sucks")
     elements.forEach((el) => {
         if (el.isIntersecting) {
-            el.target.classList.add('show');
+            if (el.target.id != "scrollHint") {
+                el.target.classList.add('show');
+            } else {
+                setTimeout(() => {
+                    console.log("I fucking love you")
+                    scrollHint.classList.add('show');
+                    
+                }, 2500);
+            }
+
         } else {
             el.target.classList.remove('show');
         }
     });
 });
 const hiddenElements = document.querySelectorAll('.hidden');
+const scrollHint = document.getElementById('scrollHint');
 hiddenElements.forEach((elmt) => observeForShowAndHidden.observe(elmt));
+
